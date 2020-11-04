f74cc369a34f6d9a31dcb111eda6eeeb601050a0
Vinícius R. Miguel <lemao.vrm07@hotmail.com>
2020-11-04 14:48:15 -0300

    Example usage

--- libklatexformula.pro
+++ libklatexformula.pro
@@ -1,8 +1,6 @@
-QT       += core gui
+QT       += core xml
 
-greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
-
-CONFIG += c++11
+CONFIG += c++11 console
 
 # The following define makes your compiler emit warnings if you use
 # any Qt feature that has been marked deprecated (the exact warnings
@@ -19,21 +17,30 @@ SOURCES += \
     main.cpp \
     src/klfbackend.cpp \
     src/klfblockprocess.cpp \
+    src/klfdatautil.cpp \
+    src/klfdebug.cpp \
     src/klfdefs.cpp \
+    src/klffactory.cpp \
     src/klffilterprocess.cpp \
     src/klflatexpreviewthread.cpp \
+    src/klfpobj.cpp \
     src/klfsysinfo.cpp \
-    src/klfuserscript.cpp
+    src/klfuserscript.cpp \
+    src/klfutil.cpp
 
 HEADERS += \
     src/klfbackend.h \
     src/klfbackend_p.h \
     src/klfblockprocess.h \
+    src/klfdatautil.h \
+    src/klfdebug.h \
     src/klfdefs.h \
+    src/klffactory.h \
     src/klffilterprocess.h \
     src/klffilterprocess_p.h \
     src/klflatexpreviewthread.h \
     src/klflatexpreviewthread_p.h \
+    src/klfpobj.h \
     src/klfsysinfo.h \
     src/klfuserscript.h \
     src/klfutil.h
--- main.cpp
+++ main.cpp
@@ -1,6 +1,42 @@
 #include "src/klfbackend.h"
+#include <qfile.h>
+#include <QCoreApplication>
 
-int main(void)
+int main(int argc, char ** argv)
 {
-    return 0;
+    QCoreApplication example(argc, argv);
+
+    KLFBackend::klfSettings settings;
+    bool ok = KLFBackend::detectSettings(&settings);
+    if (!ok) {
+        throw std::runtime_error("error in your system: are latex,dvips and gs installed?");
+        return EXIT_FAILURE;
+    }
+
+    KLFBackend::klfInput input;
+    input.latex = "\\int_{\\Sigma}\\!(\\vec{\\nabla}\\times\\vec u)\\,d\\vec S ="
+                  " \\oint_C \\vec{u}\\cdot d\\vec r";
+    input.mathmode = "\\[ ... \\]";
+    input.preamble = "\\usepackage{somerequiredpackage}\n";
+    input.fg_color = qRgb(255, 168, 88); // beige
+    input.bg_color = qRgba(0, 64, 64, 255); // dark turquoise
+    input.dpi = 300;
+
+    KLFBackend::klfOutput out = KLFBackend::getLatexFormula(input, settings);
+
+    if (out.status != 0) {
+        // an error occurred. an appropriate error string is in out.errorstr
+        fprintf(stderr, "Problem in getLatexFormula: %s", out.errorstr.toStdString().c_str());
+        return EXIT_FAILURE;
+    }
+
+    // write contents of 'out.pdfdata' to a file to get a PDF file (for example)
+    {
+        QFile fpdf("teste.pdf");
+
+        fpdf.open(QFile::WriteOnly);
+        fpdf.write(out.pdfdata);
+    }
+
+    return example.exec();
 }
